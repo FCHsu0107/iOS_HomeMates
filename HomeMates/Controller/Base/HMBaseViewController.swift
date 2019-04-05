24d1c15d6f30d4894b4b8a52df6d44d21de0c57c
Jacuqueline_Hsu <hsufuchiung@gmail.com>
2019-04-05 14:31:43 +0800

    Install Pods

---
+++ HomeMates/Controller/Base/HMBaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HMBaseViewController.swift
+//  HomeMates
+//
+//  Created by Fu-Chiung HSU on 2019/4/5.
+//  Copyright © 2019 Fu-Chiung HSU. All rights reserved.
+//
+
+import UIKit
+
+class HMBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HomeMates/VIew/Profile/ProfileHeaderViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileHeaderViewCell.swift
+//  HomeMates
+//
+//  Created by Fu-Chiung HSU on 2019/4/5.
+//  Copyright © 2019 Fu-Chiung HSU. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
