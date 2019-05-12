043444de650fc2e99a01b83c13f886c543e8b71d
Jacuqueline_Hsu <hsufuchiung@gmail.com>
2019-05-12 18:21:54 +0800

    Establish the profile info page

---
+++ HomeMates/Controller/Profile/ProfileInfoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileInfoViewController.swift
+//  HomeMates
+//
+//  Created by Fu-Chiung HSU on 2019/5/12.
+//  Copyright © 2019 Fu-Chiung HSU. All rights reserved.
+//
+
+import UIKit
+
+class ProfileInfoViewController: HMBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
